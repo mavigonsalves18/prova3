39c7c9865006aeb2a438670467ea89a7c9b46dd9
Maria Vitória <mavigonsalves@gmail.com>
2022-09-23 20:03:58 -0300

    terceira prova

---
+++ provaPda.js
@@ -0,0 +1,10 @@
+let bim1 = 6 ;
+ let bim2 = 1;
+ let bim3 = 8;
+ let bim4 = 4;
+ soma = bim1+bim2+bim3+bim4
+ media = soma/4
+ if (media >= 7) 
+ {console.log("aprovado");  } 
+ else 
+{console.log("Reprovado!")}
